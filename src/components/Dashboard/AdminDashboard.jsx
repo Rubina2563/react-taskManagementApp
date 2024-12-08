7bc41b4069e37002d42126b7368c2c96547258d5
RubinaKhan <rubinatazak@gmail.com>
2024-12-08 17:55:03 +0530

    added functionality to add and delete usr

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -3,8 +3,8 @@ import Header from "../others/Header";
 import CreateTask from "../others/CreateTask";
 import AllTask from "../others/AllTask";
 import AddUser from "../others/AddUser";
+
 const AdminDashboard = (props) => {
-  
   const [employees, setEmployees] = useState(
     JSON.parse(localStorage.getItem("employees")) || []
   );
@@ -16,8 +16,12 @@ const AdminDashboard = (props) => {
 
   return (
     <div className="bg-gray-600 min-h-screen text-gray-100">
-      <Header changedUser={props.changedUser} data={props.data} updateEmployees={updateEmployees} />
-<AddUser/>
+      <Header
+        changedUser={props.changedUser}
+        data={props.data}
+        updateEmployees={updateEmployees}
+      />
+      <AddUser updateEmployees={updateEmployees} />
       <div className="max-w-7xl mx-auto px-4 py-8 space-y-10">
         <CreateTask onTaskCreated={updateEmployees} />
         <AllTask employees={employees} />
